07496e3a610319fb21782760c34b3a511b34f554
TylerOuellette6 <touel912@mail.endicott.edu>
2019-11-27 15:30:52 -0500

    Inventory item behavior and player inventory added.

---
+++ Assets/InventoryItemBehavior.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryItemBehavior : MonoBehaviour
+{
+    public Canvas pressFToPickUp;
+    public GameObject inventoryObj;
+    public GameObject playerObj;
+
+    private bool nearInventoryObj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pressFToPickUp.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if((inventoryObj.transform.position - playerObj.transform.position).magnitude < 7.0f && !nearInventoryObj)
+        {
+            pressFToPickUp.enabled = true;
+            nearInventoryObj = true;
+        }
+        if((inventoryObj.transform.position - playerObj.transform.position).magnitude > 7.0f && nearInventoryObj)
+        {
+            pressFToPickUp.enabled = false;
+            nearInventoryObj = false;
+        }
+        if(Input.GetKeyDown(KeyCode.F) && nearInventoryObj)
+        {
+            HandleItemPickup();
+        }
+    }
+
+    public void HandleItemPickup()
+    {
+        pressFToPickUp.enabled = false;
+        WaffleInventoryManager.addItemToInventory(inventoryObj);
+        Destroy(inventoryObj);
+    }
+}
---
+++ Assets/WaffleInventoryManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaffleInventoryManager : MonoBehaviour
+{
+    static List<GameObject> inventoryItems;
+    // Start is called before the first frame update
+    void Start()
+    {
+        inventoryItems = new List<GameObject>();
+    }
+
+    public static void addItemToInventory(GameObject newInventoryItem)
+    {
+        inventoryItems.Add(newInventoryItem);
+        foreach(GameObject inventoryItem in inventoryItems)
+        {
+            Debug.Log(inventoryItem.name);
+        }
+    }
+}
